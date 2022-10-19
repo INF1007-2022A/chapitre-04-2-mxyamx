43d3a5bae5c55972f2fcb20ce5c81fbe93c3c001
maya <aitmayamanel@gmail.com>
2022-10-18 23:19:47 -0400

    4.2 resolus

--- exercice.py
+++ exercice.py
@@ -4,16 +4,30 @@
 import random
 
 def get_first_part_of_name(name):
-	return ""
+	first_part = name.split('-')[0]
+	capitalized = first_part[0].upper() + first_part[1:].lower()
+	return (f"Bonjour, {capitalized}")
 
 def get_random_sentence(animals, adjectives, fruits):
-	return ""
+	animals = random.choice(animals)
+	adjectives = random.choice(adjectives)
+	fruits = random.choice(fruits)
+	return (f"Aujourd’hui, j’ai vu un {animals} s’emparer d’un panier {adjectives} plein de {fruits}. ")
 
 def encrypt(text, shift):
-	return ""
+	result = ""
+	for letter in text:
+		encrypted_letter = letter
+		if letter.isalpha():
+			index = ord(letter.upper()) - ord("A")
+			encrypted_index = (index + shift) % 26
+			encrypted_letter = chr(ord("A") + encrypted_index)
+		result += encrypted_letter
+	return result
+
 
 def decrypt(encrypted_text, shift):
-	return ""
+	return encrypt(encrypted_text, -shift)
 
 
 if __name__ == "__main__":
@@ -24,7 +38,7 @@ if __name__ == "__main__":
 	adjectives = ("rouge", "officiel", "lourd")
 	fruits = ("pommes", "kiwis", "mangue")
 	print(get_random_sentence(animals, adjectives, fruits))
-	
+
 	print(encrypt("ABC", 1))
 	print(encrypt("abc 123 XYZ", 3))
 	print(decrypt("DEF 123 ABC", 3))
